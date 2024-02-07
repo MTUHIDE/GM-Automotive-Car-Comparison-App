48731921c45ec1e66d2d5630c5cf79b3af33422d
Chanpech <70418825+Chanpech@users.noreply.github.com>
2024-02-07 16:07:48 -0500

    Initialized vite.js and react

---
+++ car-comparison-nextjs/app/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <h1>Welcome to the Main Page</h1>
+            {/* Add your content here */}
+        </div>
+    );
+};
+
+export default Main;
