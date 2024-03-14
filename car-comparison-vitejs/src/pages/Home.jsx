a7e8c4e2a2485d61d4993f3f21b73d3401fb418d
Meowtomata <its.anatoliy@gmail.com>
2024-03-14 12:46:49 -0400

    Add car comparison boxes

--- car-comparison-vitejs/src/pages/Home.jsx
+++ car-comparison-vitejs/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import Navbar from '../components/Nav/Navbar.jsx'
 import Footer from '../components/Footer/Footer.jsx'
+import CompareCarBox from '../components/CompareCarBox/CompareCarBox.jsx'
 
 
 export default function Home() {
     return (
         <div>
             <Navbar />
-            <h3>Home</h3>
+            <h3 className="car-comparison-h3">Car Comparison Tool</h3>
+            <CompareCarBox />
+            <button className="compare-button">Compare</button>
         </div>
     )
 }
