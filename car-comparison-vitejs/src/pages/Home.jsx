edefa586b1da4c32a381588a298203ea76fa953f
Meowtomata <its.anatoliy@gmail.com>
2024-03-14 21:05:00 -0400

    More CSS changes to reflect original design

--- car-comparison-vitejs/src/pages/Home.jsx
+++ car-comparison-vitejs/src/pages/Home.jsx
@@ -1,13 +1,12 @@
 import Navbar from '../components/Nav/Navbar.jsx'
-import Footer from '../components/Footer/Footer.jsx'
 import CompareCarBox from '../components/CompareCarBox/CompareCarBox.jsx'
-
+import './Home.css'
 
 export default function Home() {
     return (
         <div className="home">
             <Navbar />
-            <h3 className="car-comparison-h3">Car Comparison Tool</h3>
+            <h1 className="car-comparison-h1">Select Two Vehicles to Compare</h1>
             <CompareCarBox />
             <div className="compare-button-container">
                 <button className="compare-button">Compare</button>
