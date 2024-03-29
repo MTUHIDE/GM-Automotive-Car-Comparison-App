00d4ac8beefd5903f7a56b40a571bcb0856c2d68
Chanpech <70418825+Chanpech@users.noreply.github.com>
2024-03-29 16:07:09 -0400

    Minor Updates
    Added footer component into the pages.

--- car-comparison-vitejs/src/pages/Home.jsx
+++ car-comparison-vitejs/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Navbar from '../components/Nav/Navbar.jsx'
 import CompareCarBox from '../components/CompareCar/CompareCarBox.jsx'
+import Footer from '../components/Footer/Footer.jsx'
 
 export default function Home() {
     return (
@@ -7,6 +8,7 @@ export default function Home() {
             <Navbar />
             <h1 className="car-comparison-h1">Select Two Vehicles to Compare</h1>
             <CompareCarBox />
+            <Footer />
         </>
     )
 }
